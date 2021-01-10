e6baf7272dc7a650e84aa2458ba5a462efac82a8
Georges <georges.alkhouri@gmail.com>
2021-01-10 10:55:47 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 case "$OSTYPE" in
-  darwin*)  echo "OSX" ;; 
+  darwin*)  sudo xcode-select --install 
+            /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   linux*)   echo "LINUX" ;;
   *)        echo "$OSTYPE not supported" ;;
 esac
