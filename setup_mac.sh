4fde3351179c0840e3d3a2cfaf09cdace64ed255
Georges <georges.alkhouri@gmail.com>
2021-01-12 19:32:54 +0100

    Update setup_mac.sh

--- setup_mac.sh
+++ setup_mac.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+set -e
+set -x
+
+if ! [ -x "$(command -v xcode-select)" ]; then
+  sudo xcode-select --install  
+fi
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" ;;
